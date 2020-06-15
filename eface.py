8060fd6cc007cd3103c95eb01191c9f7da2617cc
yihang <huyihang99@gmail.com>
2020-06-15 15:44:59 -0400

    env

---
+++ eface.py
@@ -0,0 +1,149 @@
+# This program generate multi view random face dataset under 3DMM model
+
+import torch
+import pyredner
+import h5py
+import os
+import numpy as np
+os.chdir('..')
+
+name = 'ones'
+
+os.system('rm -rf generated/dataset_' + name)
+
+# Load the Basel face model
+shape_mean, shape_basis, triangle_list, color_mean, color_basis = np.load("3dmm.npy", allow_pickle=True)
+indices = triangle_list.permute(1, 0).contiguous()
+print("finish loading")
+
+def model(cam_poses, cam_look_at, shape_coeffs, color_coeffs, lights_list, resolution):
+    vertices = (shape_mean + shape_basis @ shape_coeffs).view(-1, 3)
+    normals = pyredner.compute_vertex_normal(vertices, indices)
+    colors = (color_mean + color_basis @ color_coeffs).view(-1, 3)
+    #m = pyredner.Material(diffuse_reflectance = torch.tensor([0.5, 0.5, 0.5]))
+    m = pyredner.Material(use_vertex_color=True)
+    obj = pyredner.Object(vertices=vertices, indices=indices, normals=normals, material=m, colors=colors)
+
+    imgs = []
+    for i in range(cam_poses.size(0)):
+        cam = pyredner.Camera(position=cam_poses[i],
+                              look_at=cam_look_at,  # Center of the vertices
+                              up=torch.tensor([0.0, 1.0, 0.0]),
+                              fov=torch.tensor([45.0]),
+                              resolution=resolution)
+        scene = pyredner.Scene(camera=cam, objects=[obj])
+        #ambient_light = pyredner.AmbientLight(ambient_color)
+        #dir_light = pyredner.DirectionalLight(dir_light_direction, dir_light_intensity)
+        img = pyredner.render_deferred(scene=scene, lights=lights_list[i % len(lights_list)], aa_samples=1)
+        imgs.append(img)
+    return imgs, obj
+
+
+dir_light_directions = torch.tensor([[-1.0, -1.0, -1.0],
+                                    [1.0, -0.0, -1.0],
+                                    [0.0, 0.0, -1.0]])
+dir_light_intensity = torch.ones(3) / 2
+
+lights_list = [[pyredner.DirectionalLight(dir_light_directions[i], dir_light_intensity)] for i in range(3)]
+
+cam_poses = torch.tensor([[-0.2697, -5.7891, 350.9277],
+                          [-240.2697, -5.7891, 240.9277],
+                          [240.2697, -5.7891, 240.9277],
+                          [-100.2697, -95.7891, 320.9277],
+                          [-100.2697, 85.7891, 320.9277],
+                          [100.2697, -95.7891, 320.9277],
+                          [100.2697, 85.7891, 320.9277],
+                          [-0.2697, -5.7891, 350.9277],
+                          [-240.2697, -5.7891, 240.9277],
+                          [240.2697, -5.7891, 240.9277],
+                          [-100.2697, -95.7891, 320.9277],
+                          [-100.2697, 85.7891, 320.9277],
+                          [100.2697, -95.7891, 320.9277],
+                          [100.2697, 85.7891, 320.9277]])
+
+cam_poses = cam_poses[:7]
+
+cam_look_at = torch.tensor([-0.2697, -5.7891, 54.7918])
+resolution = (1000, 1000)
+env_data = np.array((cam_poses, cam_look_at, lights_list))
+
+#img = model(cam_pos, cam_look_at, torch.ones(199, device=pyredner.get_device()), torch.ones(199, device=pyredner.get_device()), torch.ones(3), torch.zeros(3))
+#pyredner.imwrite(img.cpu(), 'img.png')
+import numpy.random as nprd
+
+
+shape_coe = 25 * torch.ones(199, device=pyredner.get_device()) #torch.randn(199, device=pyredner.get_device(), dtype=torch.float32)
+color_coe = 3 * torch.ones(199, device=pyredner.get_device()) #torch.tensor(3 * nprd.randn(199), device=pyredner.get_device(), dtype=torch.float32)
+
+imgs, obj = model(cam_poses, cam_look_at, shape_coe, color_coe, lights_list, (1000, 1000))
+for i in range(len(imgs)):
+    pyredner.imwrite(imgs[i].cpu(), 'generated/dataset_' + name + '/tgt_img{:0>2d}.png'.format(i))
+
+obj.material = pyredner.Material(diffuse_reflectance=torch.tensor([0.5, 0.5, 0.5]))
+pyredner.save_obj(obj, 'generated/dataset_' + name + '/' + name + '.obj')
+np.save('generated/dataset_' + name + '/env_data.npy', env_data)
+
+
+'''
+import urllib
+
+#urllib.request.urlretrieve('https://raw.githubusercontent.com/BachiLi/redner/master/tutorials/mona-lisa-cropped-256.png', 'target.png')
+target = pyredner.imread('target.png').to(pyredner.get_device())
+cam_pos = torch.tensor([-0.2697, -5.7891, 373.9277], requires_grad=True)
+cam_look_at = torch.tensor([-0.2697, -5.7891, 54.7918], requires_grad=True)
+shape_coeffs = torch.zeros(199, device=pyredner.get_device(), requires_grad=True)
+color_coeffs = torch.zeros(199, device=pyredner.get_device(), requires_grad=True)
+ambient_color = torch.ones(3, device=pyredner.get_device(), requires_grad=True)
+dir_light_intensity = torch.zeros(3, device=pyredner.get_device(), requires_grad=True)
+
+optimizer = torch.optim.Adam([shape_coeffs, color_coeffs, ambient_color, dir_light_intensity], lr=0.1)
+cam_optimizer = torch.optim.Adam([cam_pos, cam_look_at], lr=0.5)
+
+import matplotlib.pyplot as plt
+#% matplotlib inline
+from IPython.display import display, clear_output
+import time
+
+plt.figure()
+imgs, losses = [], []
+# Run 500 Adam iterations
+num_iters = 500
+for t in range(num_iters):
+    optimizer.zero_grad()
+    cam_optimizer.zero_grad()
+    img = model(cam_pos, cam_look_at, shape_coeffs, color_coeffs, ambient_color, dir_light_intensity)
+    # Compute the loss function. Here it is L2 plus a regularization term to avoid coefficients to be too far from zero.
+    # Both img and target are in linear color space, so no gamma correction is needed.
+    loss = (img - target).pow(2).mean()
+    loss = loss + 0.0001 * shape_coeffs.pow(2).mean() + 0.001 * color_coeffs.pow(2).mean()
+    loss.backward()
+    optimizer.step()
+    cam_optimizer.step()
+    ambient_color.data.clamp_(0.0)
+    dir_light_intensity.data.clamp_(0.0)
+    # Plot the loss
+    f, (ax_loss, ax_diff_img, ax_img) = plt.subplots(1, 3)
+    losses.append(loss.data.item())
+    # Only store images every 10th iterations
+    if t % 10 == 0:
+        imgs.append(torch.pow(img.data, 1.0 / 2.2).cpu())  # Record the Gamma corrected image
+        pyredner.imwrite(imgs[t // 10].cpu(), 'process/img{}.png'.format(t // 10))
+    clear_output(wait=True)
+    ax_loss.plot(range(len(losses)), losses, label='loss')
+    ax_loss.legend()
+    ax_diff_img.imshow((img - target).pow(2).sum(dim=2).data.cpu())
+    ax_img.imshow(torch.pow(img.data.cpu(), 1.0 / 2.2))
+    plt.show()
+
+
+'''
+
+
+
+
+
+
+
+
+
+
---
+++ etrecon.py
@@ -0,0 +1,267 @@
+# This program reconstruct the face from multi images and try to smooth
+# output form changed
+
+import torch
+import pyredner
+import numpy as np
+import os
+import sys
+import argparse
+
+description = 'change rsl'
+
+#<editor-fold desc="PARSE ARGUMENTS">
+parser = argparse.ArgumentParser(prog='face recon',
+                                 description='reconstruct the face from images',
+                                 epilog='enjoy!')
+parser.add_argument('-ns',
+                    '--normal_scheme',
+                    action='store',
+                    choices=['max', 'cotangent'],
+                    type=str,
+                    help='scheme of computing vertex normal',
+                    default='max')
+parser.add_argument('-ss',
+                    '--smooth_scheme',
+                    action='store',
+                    choices=['uniform', 'reciprocal', 'cotangent', 'none'],
+                    type=str,
+                    help='scheme of smoothing',
+                    default='uniform')
+parser.add_argument('-lmd',
+                    '--smooth_lmd',
+                    action='store',
+                    type=float,
+                    help='lambda value in smoothing',
+                    default=0.5)
+parser.add_argument('-p',
+                    '--output_path',
+                    action='store',
+                    type=str,
+                    help='path to output results',
+                    default='p_default')
+parser.add_argument('-ni',
+                    '--num_iters_1',
+                    action='store',
+                    type=int,
+                    help='number of iterations',
+                    default=30)
+
+args = parser.parse_args()
+normal_scheme = args.normal_scheme
+smooth_scheme = args.smooth_scheme
+smooth_lmd = args.smooth_lmd
+output_path = args.output_path
+num_iters_1 = args.num_iters_1
+print(vars(args))
+
+#</editor-fold>
+
+os.chdir('..')
+os.system("rm -rf " + output_path)
+
+pyredner.set_print_timing(False)
+
+#shape_mean, shape_basis, triangle_list, color_mean, color_basis = np.load("3dmm.npy", allow_pickle=True)
+#indices = triangle_list.permute(1, 0).contiguous()
+#vertices = shape_mean.view(-1, 3)
+obj = pyredner.load_obj("init/final.obj", return_objects=True)[0]
+indices = obj.indices.detach()
+vertices = obj.vertices.detach()
+
+tex = obj.material.diffuse_reflectance.texels
+
+texels = pyredner.imresize(tex, (200, 200))
+print('texels size: ', texels.size())
+texels.requires_grad = True
+m = pyredner.Material(diffuse_reflectance=texels)
+
+vertices.requires_grad = True
+
+target_data_path = "generated/dataset_ones/"
+cam_poses, cam_look_at, lights_list = np.load(target_data_path + "env_data.npy", allow_pickle=True)
+#cam_poses = cam_poses[:1]
+
+uvs = obj.uvs
+uv_indices = obj.uv_indices
+
+def model(cam_poses,
+          cam_look_at,
+          vertices,
+          lights_list,
+          normals,
+          material):
+
+    # m = pyredner.Material(diffuse_reflectance=torch.tensor([0.5, 0.5, 0.5]))
+    obj = pyredner.Object(vertices=vertices,
+                          indices=indices,
+                          normals=normals,
+                          material=material,
+                          uvs=uvs,
+                          uv_indices=uv_indices)  # , colors=colors)
+    imgs = []
+    for i in range(cam_poses.size(0)):
+        cam = pyredner.Camera(position=cam_poses[i],
+                              look_at=cam_look_at,  # Center of the vertices
+                              up=torch.tensor([0.0, 1.0, 0.0]),
+                              fov=torch.tensor([45.0]),
+                              resolution=(1000, 1000))
+        scene = pyredner.Scene(camera=cam, objects=[obj])
+        img = pyredner.render_deferred(scene=scene, lights=lights_list[i % len(lights_list)], aa_samples=1)
+        imgs.append(img)
+    return imgs
+
+
+target = []
+for i in range(len(cam_poses)):
+    target.append(pyredner.imread(target_data_path + 'tgt_img{:0>2d}.png'.format(i)).to(pyredner.get_device()))
+    # pyredner.imwrite(target[i].cpu(), output_path + '/target_img{:0>2d}.png'.format(i))
+
+ambient_color = torch.zeros(3, device=pyredner.get_device(), requires_grad=False)
+
+bound = 1. * pyredner.bound_vertices(vertices, indices) - 0.
+boundary = vertices.data * (1. - bound).reshape(-1, 1).expand(-1, 3)
+ver_optimizer = torch.optim.Adam([vertices], lr=0.2)
+tex_optimizer = torch.optim.Adam([texels], lr=0.05)
+
+all_losses, img_losses, smooth_losses, total_losses, all_texels, all_imgs = [], [], [], [], [], []
+
+
+
+print('Finish loading')
+
+for t in range(num_iters_1):
+
+    ver_optimizer.zero_grad()
+    tex_optimizer.zero_grad()
+    normals = pyredner.compute_vertex_normal(vertices, indices, normal_scheme)
+
+    m = pyredner.Material(diffuse_reflectance=texels)
+    imgs = model(cam_poses, cam_look_at, vertices, lights_list, normals, m)
+    # imgs of all viewpoints
+    all_imgs.append(imgs)
+    # record all imgs
+    losses = torch.stack([(imgs[i] - target[i]).pow(2).mean() for i in range(len(imgs))])
+    # losses of all imgs in this single iteration
+    all_losses.append(losses)
+    # all_losses records the losses in all iterations
+    img_loss = losses.sum()
+
+    smooth_loss = 0.1 * pyredner.smooth(vertices, indices, 0., smooth_scheme, bound, True)
+
+    total_loss = img_loss # + smooth_loss
+    total_losses.append(total_loss)
+    img_losses.append(img_loss)
+    smooth_losses.append(smooth_loss)
+    all_texels.append(texels.data.cpu())
+
+    total_loss.backward()
+    ver_optimizer.step()
+    tex_optimizer.step()
+    vertices.data = vertices.data * bound.reshape(-1, 1).expand(-1, 3) + boundary
+
+    if 1:#smooth_scheme != 'none':  # and t > 20:smi
+        for num_of_smooth in range(2):
+            pyredner.smooth(vertices, indices, smooth_lmd, smooth_scheme, bound)
+            pyredner.smooth(vertices, indices, -smooth_lmd, smooth_scheme, bound)
+
+    print("{:.^8}total_loss:{:.6f}...img_loss:{:.6f}...smooth_loss:{:.6f}".format(t, total_loss, img_loss, smooth_loss))
+
+    if 0:#(2 * (t + 1)) % num_iters_1 == 0 and t + 1 < num_iters_1:
+        print(t)
+        texels = pyredner.imresize(texels, scale=2.).detach()
+        texels.requires_grad = True
+        tex_optimizer = torch.optim.Adam([texels], lr=0.05)
+        print(texels.size())
+
+all_losses = torch.stack(all_losses).detach().cpu()
+total_losses = torch.stack(total_losses).detach().cpu()
+img_losses = torch.stack(img_losses).detach().cpu()
+smooth_losses = torch.stack(smooth_losses).detach().cpu()
+
+print()
+
+obj = pyredner.Object(vertices=vertices, indices=indices, normals=normals, material=m, uvs=uvs, uv_indices=uv_indices)  # , colors=colors)
+pyredner.save_obj(obj, output_path + '/final.obj')
+#pyredner.imwrite(texels.data.cpu(), output_path + '/texels.png')
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+plt.suptitle(description + '\n' + str(vars(args))[1:-1].replace("'", ""), fontsize=13, color='blue')
+
+plt.sca(ax1)
+
+imgs = model(cam_poses, cam_look_at, vertices, lights_list, normals, m)
+
+for i in range(len(cam_poses)):
+    pyredner.imwrite(imgs[i].data.cpu(), output_path + '/final_views/view{:0>2d}.png'.format(i))
+    plt.plot(all_losses[:, i], label='view{:0>2d}:{:.6f}'.format(i, all_losses[-1, i]))
+
+plt.legend()
+plt.ylabel("losses")
+plt.xlabel("iterations")
+
+plt.ylim(ymin=0.)
+xlim = plt.xlim()
+ylim = plt.ylim()
+
+plt.sca(ax2)
+plt.plot(total_losses, label="total_loss:{:.6f}".format(total_losses[-1]))
+plt.plot(img_losses, label="img_loss:{:.6f}".format(img_losses[-1]))
+plt.plot(smooth_losses, label="smooth_loss:{:.6f}".format(smooth_losses[-1]))
+plt.legend()
+plt.xlabel("iterations")
+plt.ylim(ymin=0.)
+xxlim = plt.xlim()
+yylim = plt.ylim()
+plt.savefig(output_path + "/lossCurve.png", dpi=800)
+
+print("lossCurve printed")
+
+from matplotlib import animation
+
+for i in range(len(cam_poses)):
+
+    fig, (img_plot, diff_plot, loss_curve) = plt.subplots(1, 3, figsize=(16, 8))
+
+    plt.suptitle(description + '\n' + str(vars(args))[1:-1].replace("'", ""), fontsize=16, color='blue')
+
+    im = img_plot.imshow(torch.pow(all_imgs[0][i], 1.0 / 2.2).clamp(0.0, 1.0).detach().cpu(), animated=True)
+    im_diff = diff_plot.imshow(abs(all_imgs[0][i] - target[i]).clamp(0.0, 1.0).detach().cpu(), animated=True)
+    loss_curve.set_xlim(xlim)
+    loss_curve.set_ylim(ylim)
+    lc, = loss_curve.plot([], [], 'b')
+
+    def update_fig(x):
+        im.set_array(torch.pow(all_imgs[x][i], 1.0 / 2.2).clamp(0.0, 1.0).detach().cpu())
+        im_diff.set_array(abs(all_imgs[x][i] - target[i]).clamp(0.0, 1.0).detach().cpu())
+        lc.set_data(range(x), all_losses[:x, i])
+        return im, im_diff, lc
+
+    anim = animation.FuncAnimation(fig, update_fig, frames=len(all_imgs), interval=300, blit=True)
+    anim.save(output_path + '/anim{:0>2d}.gif'.format(i), writer='imagemagick')
+    print('anim{:0>2d}.gif generated'.format(i))
+
+
+
+fig, (tex_plot, loss_curve) = plt.subplots(1, 2, figsize=(16, 8))
+
+plt.suptitle(description + '\n' + str(vars(args))[1:-1].replace("'", ""), fontsize=16, color='blue')
+
+im = tex_plot.imshow(all_texels[i].clamp(0.0, 1.0), animated=True)
+loss_curve.set_xlim(xxlim)
+loss_curve.set_ylim(yylim)
+lc, = loss_curve.plot([], [], 'b')
+
+def update_fig(x):
+    im.set_array(all_texels[x].clamp(0.0, 1.0))
+    lc.set_data(range(x), total_losses[:x])
+    return im, lc
+
+anim = animation.FuncAnimation(fig, update_fig, frames=len(all_texels), interval=300, blit=True)
+anim.save(output_path + '/anim_texels.gif'.format(i), writer='imagemagick')
+print('anim_texels.gif generated'.format(i))
+
+print("Finish running!")
